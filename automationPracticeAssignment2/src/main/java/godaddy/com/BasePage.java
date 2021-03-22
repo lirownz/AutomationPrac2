f77b46b1a216896a5b1427b07225e4474910600d
lirownz <lironagam@gmail.com>
2021-03-22 13:14:44 +0200

    new code after sitting with Margalit on Erez notes

---
+++ automationPracticeAssignment2/src/main/java/godaddy/com/BasePage.java
@@ -0,0 +1,39 @@
+package godaddy.com;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+
+import org.testng.Assert;
+
+public class BasePage{
+
+	public WebDriver driver;
+
+	//Constructor
+	public BasePage (WebDriver driver){
+		this.driver = driver;
+	}
+
+	//	//Constructor for the godaddySite Driver
+	//	public BasePage (){
+	//	}
+
+	public void assertEqualsText (String actualTitle, String expectedTitle) {
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+
+	public void assertTrueIfContain(boolean inPageSource) {
+		Assert.assertTrue(inPageSource);
+	}
+
+	//window resize
+	public void reSizeWindow(){
+		// dimension the Chrome window to 800x600
+		Dimension dimension = new Dimension(800, 600);
+		driver.manage().window().setSize(dimension);
+		// full screen the Chrome window
+		driver.manage().window().fullscreen();
+		// maximize the Chrome window
+		driver.manage().window().maximize();
+	}
+}
---
+++ automationPracticeAssignment2/src/main/java/godaddy/com/BaseTest.java
@@ -0,0 +1,32 @@
+package godaddy.com;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest{
+
+	public WebDriver driver;
+
+	@BeforeClass
+	public void setUpTest() {
+		//setting Chrome driver from Maven  
+		WebDriverManager.chromedriver().setup();
+		//Create a Chrome driver. All test classes use this.
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//driver.navigate().to("https://www.godaddy.com/");
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void teardown () {
+		//setting Chrome driver to quit after finishing all tests
+		driver.quit();
+		System.out.println("Test Completed");
+	}
+}
